4ccbbf7d4a554104d8b0926f8ed3e95dfbc29c66
bailey watkins <bailey.watkins@rocketmail.com>
2021-04-26 18:15:32 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+//let name = [ 'Lisa', '/Kaitlin', 'Jan' ]
+
+function writeCards(array){
+    
+   let cards = [];
+   for(let i = 0; i < array.length; i++){
+   cards.push (`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+}
+return cards; 
+}
+
+function countDown(){
+   let x = 10;
+   while(x>=0){
+      console.log(x);
+      x--; 
+} 
+}
+countDown();
+
